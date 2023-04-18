a33b7098a6f2c98eb3a28e8e3840bf10db7ae86d
C0B22152 <c0b2215229@edu.teu.ac.jp>
2023-04-18 17:17:20 +0900

    演習課題２

--- pygame_image.py
+++ pygame_image.py
@@ -8,8 +8,9 @@ def main():
     bg_img = pg.image.load("ex01-20230418/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01-20230418/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
-    kk_lst = [kk_img,pg.transform.rotozoom(kk_img,-5,1.0),pg.transform.rotozoom(kk_img,5,1.0)]
-                                            
+    kk_lst = [kk_img,pg.transform.rotozoom(kk_img,10,1.0),pg.transform.rotozoom(kk_img,-10,1.0)]
+    bg_img2 = pg.transform.flip(bg_img,True,False)
+
     tmr = 0
 
     while True:
@@ -17,9 +18,10 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        x = tmr%1600
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img,[1600-x,0])
+        screen.blit(bg_img2,[1600-x,0])
+        screen.blit(bg_img,[3200-x,0])
         screen.blit(kk_lst[(tmr//50)%2],[300,200])
 
         pg.display.update()
